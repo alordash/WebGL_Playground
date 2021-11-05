4e87e0a6cf26a849996730058dd353817df4029e
alordash <alordash@mail.ru>
2021-11-05 15:17:54 +0300

    Added Misc.cs, adding VoronoiTesselation.razor

---
+++ WebGL_Playground/WebGL_Playground_Site/Model/Geometry/Primitives.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+
+namespace WebGL_Playground_Site.Geometry {
+    public class Point {
+        public float x;
+        public float y;
+
+        public Point(float x, float y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        public static IEnumerable<Point> FloatsToPoints(IEnumerable<float> values) {
+            if ((values.Count() & 1) == 1) {
+                return Enumerable.Empty<Point>();
+            }
+
+            return values.Zip(values.Skip(1)).Select(x => new Point(x.First, x.Second));
+        }
+    }
+}
---
+++ WebGL_Playground/WebGL_Playground_Site/Model/Misc.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace WebGL_Playground_Site {
+    public static class Misc {
+        public static async Task<String> GetFileContents(string sourcePath, HttpClient http) {
+            string source;
+            try {
+                source = await http.GetStringAsync(sourcePath);
+                return source;
+            } catch (Exception e) {
+                throw new Exception(e.Message);
+            }
+        }
+    }
+}
