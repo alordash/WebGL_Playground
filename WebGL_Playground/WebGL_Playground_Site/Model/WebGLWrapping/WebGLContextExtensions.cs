d62b45837de4500141b5cd17174fe4453b09e804
alordash <alordash@mail.ru>
2021-11-06 01:09:51 +0300

    Fixed glTexImage2D

--- WebGL_Playground/WebGL_Playground_Site/Model/WebGLWrapping/WebGLContextExtensions.cs
+++ WebGL_Playground/WebGL_Playground_Site/Model/WebGLWrapping/WebGLContextExtensions.cs
@@ -2,16 +2,42 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Blazor.Extensions;
 using Blazor.Extensions.Canvas.WebGL;
+using Microsoft.JSInterop;
 
 namespace WebGL_Playground_Site.WebGLWrapping {
-	public static class WebGLContextExtensions {
-		public static async Task LinkProgramAsync(this WebGLContext gl, GLProgram glProgram) {
-			await gl.LinkProgramAsync(glProgram.Program);
-		}
+    public static class WebGLContextExtensions {
+        public static async Task LinkProgramAsync(this WebGLContext gl, GLProgram glProgram) {
+            await gl.LinkProgramAsync(glProgram.Program);
+        }
 
-		public static async Task UseProgramAsync(this WebGLContext gl, GLProgram glProgram) {
-			await gl.UseProgramAsync(glProgram.Program);
-		}
-	}
+        public static async Task UseProgramAsync(this WebGLContext gl, GLProgram glProgram) {
+            await gl.UseProgramAsync(glProgram.Program);
+        }
+
+        public static async Task TexImage2DAsync<T>(this WebGLContext gl,
+                                                 IJSRuntime js,
+                                                 BECanvasComponent canvasReference,
+                                                 Texture2DType target,
+                                                 uint level,
+                                                 PixelFormat internalFormat,
+                                                 uint width,
+                                                 uint height,
+                                                 uint border,
+                                                 PixelFormat format,
+                                                 PixelType type,
+                                                 T[] pixels)
+            => await js.InvokeVoidAsync("glTexImage2D",
+                                        canvasReference,
+                                        target,
+                                        level,
+                                        internalFormat,
+                                        width,
+                                        height,
+                                        border,
+                                        format,
+                                        type,
+                                        pixels);
+    }
 }
--- WebGL_Playground/WebGL_Playground_Site/wwwroot/js/JSFuncs.js
+++ WebGL_Playground/WebGL_Playground_Site/wwwroot/js/JSFuncs.js
@@ -1,7 +1,17 @@
-﻿window.glTexImage2D = async (canvas, target, level, internalFormat, width, height, border, format, type, pixels) => {
-    console.log(JSON.stringify(canvas));
+﻿window.StringToUint8Array = (str) => {
+    str = atob(str);
+    console.log(`atob(str) = "${str}"`);
+    let arr = [];
+    for(let i = 0; i < str.length; i++) {
+        arr[i] = str.charCodeAt(i);
+    }
+    return new Uint8Array(arr);
+};
+
+window.glTexImage2D = async (canvas, target, level, internalFormat, width, height, border, format, type, pixels) => {
+    console.log(`pixels = ${JSON.stringify(pixels)}`);
     let gl = canvas.canvasReference.getContext("webgl");
-    let values = new Uint8Array(pixels);
+    let values = typeof(pixels) == "string" ? StringToUint8Array(pixels) : new Uint8Array(pixels);
     await gl.texImage2D(target, level, internalFormat, width, height, border, format, type, values);
-    return JSON.stringify(values);
+    console.log(`values = ${values}`);
 };
