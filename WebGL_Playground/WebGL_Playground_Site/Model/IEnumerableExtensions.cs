882d7a849f4f9c6e913ba24359c6a1855beaf509
alordash <alordash@mail.ru>
2021-11-06 21:13:57 +0300

    Added IEnumerable.SelectTwo

--- WebGL_Playground/WebGL_Playground_Site/Model/IEnumerableExtensions.cs
+++ WebGL_Playground/WebGL_Playground_Site/Model/IEnumerableExtensions.cs
@@ -1,11 +1,22 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 
 namespace WebGL_Playground_Site {
     public static class EnumerableExtensions {
-        public static IEnumerable<T> Subsequence<T>(this IEnumerable<T> arr, int startIndex, int count) {
+        public static IEnumerable<TSource> Subsequence<TSource>(this IEnumerable<TSource> arr, int startIndex, int count) {
             return arr.Skip(startIndex).Take(count);
+        }
+
+        public static IEnumerable<TResult> SelectTwo<TSource, TResult>(this IEnumerable<TSource> arr, [NotNull] Func<TSource, TSource, TResult> selector) {
+            using var en = arr.GetEnumerator();
+            while (en.MoveNext()) {
+                var first = en.Current;
+                en.MoveNext();
+                yield return selector(first, en.Current);
+            }
         }
     }
 }
